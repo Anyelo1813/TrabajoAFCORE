9207aeb1f99d762ed390bc27d02f03f81929460f
Anyelo1813 <anyelodelarosa821@gmail.com>
2025-03-15 07:24:36 +0500

    apartado de empleados aplicado

---
+++ ERP.Web/Domain/Dto/IEmpleadoService.cs
@@ -0,0 +1,15 @@
+﻿using ERP.Web.Domain.Dto;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ERP.Web.Domain.Dto
+{
+    public interface IEmpleadoService
+    {
+        Task<List<EmpleadoDto>> Consultar(string filtro);
+        Task<bool> Crear(EmpleadoDto request);
+        Task<bool> Modificar(EmpleadoDto request);
+        Task<bool> Eliminar(int Id);
+    }
+}
+
--- ERP.Web/Program.cs
+++ ERP.Web/Program.cs
@@ -1,29 +1,33 @@
 using ERP.Web.Components;
 using ERP.Web.Data;
+using ERP.Web.Domain.Dto;
 using ERP.Web.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
-//Data Services
+
+// Data Services
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-//Agregar el servico para la gestion de los clientes
+
+
 builder.Services.AddScoped<IClienteService, ClienteService>();
+builder.Services.AddScoped<IEmpleadoService, EmpleadoService>(); // <-- Agregado
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
-
 app.UseStaticFiles();
 app.UseAntiforgery();
 
